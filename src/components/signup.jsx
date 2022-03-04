eb6d98229a84d4813d087c1dd6b3306759f1a924
Savitha G <85541504+Savisavi29@users.noreply.github.com>
2022-03-04 14:58:50 +0530

    update signup.jsx

--- src/components/signup.jsx
+++ src/components/signup.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { Component, useEffect, useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
@@ -116,53 +115,6 @@ function SignUp() {
             </Form>
         </div>
     );
-=======
-import React, { Component } from "react";
-export default class SignUp extends Component {
-    render() {
-        return (
-            <form className="main container" style={{ width: "30%" }}>
-                <h3>Sign Up</h3>
-                <div className="form-group">
-                    < label className="main-text">First name</ label >
-                    <input type="text" className="form-control" placeholder="First name" />
-                </div>
-                <div className="form-group">
-                    < label className="main-text" >Last name</ label >
-                    <input type="text" className="form-control" placeholder="Last name" />
-                </div>
-                <div className="form-group">
-                    < label className="main-text">Email address</ label >
-                    <input type="email" className="form-control" placeholder="Enter email" />
-                </div>
-                <div className="form-group">
-                    < label className="main-text">Password</label>
-                    <input type="password" className="form-control" placeholder="Enter password" />
-                </div>
-                <div className="form-group">
-                    < label className="main-text">Confirm Password</label>
-                    <input type="password" className="form-control" placeholder="Enter password again" />
-                </div>
-                <div className="form-group">
-                    < label className="main-text">DOB</label>
-                    <input type="date" className="form-control" placeholder="dd-mm-yyyy" />
-                </div>
-                <div className="form-group">
-                    < label className="main-text">Address</label>
-                    <input type="text" className="form-control" placeholder="Your Address" />
-                </div>
-                <div className="form-group">
-                    < label className="main-text">Mobile number</label>
-                    <input type="tel" className="form-control" placeholder="Mobile number" />
-                </div>
-                <button type="submit" className="btn btn-primary btn-block">Sign Up</button>
-                <p className="forgot-password text-right">
-                    Already registered <a href="/#">Log in?</a>
-                </p>
-            </form>
-        );
-    }
->>>>>>> cd2c632b2f8393fcc369a45a4240644a89726d82
+
 }
 export { SignUp };
-
